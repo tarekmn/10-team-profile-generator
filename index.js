84dba59ac69cfc74e787a113b3936e2baf1ac370
tarekmn <nytarek@gmail.com>
2022-10-11 17:00:20 -0500

    async functions

--- index.js
+++ index.js
@@ -1,33 +1,52 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const htmlData = require('./htmldata');
-const employee = require('./lib/employee');
-const engineer = require('./lib/engineer');
-const intern = require('./lib/intern');
-const manager = require('./lib/manager');
+const Employee = require('./lib/employee');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const Manager = require('./lib/manager');
 
 
-  // TODO: Create a function to write HTML file
-  function writeToFile(data) {
 
 
-    fs.writeFile('index.html', data, (err)=>
-    err ? console.log("error") : console.log('HTML Created!'))
+const engineer1 = new Engineer('tarek',  '12', 'sdf@', 'git');
+const intern1 = new Intern('sdf', '10', 'sdf', 'yt');
+const manager1 = new Intern('man1', '19', 'fgh', 'yui');
+
+
+// console.log(engineer1)
+// console.log(intern1)
+// console.log(manager1)
+
+  // // TODO: Create a function to write HTML file
+  // function writeToFile(data) {
+
+
+  //   fs.writeFile('index.html', data, (err)=>
+  //   err ? console.log("error") : console.log('HTML Created!'))
   
-  }
+  // }
 
+
+//Employee = name, employeeID, email
 //Team manager = name, employeeID, email, office#
 //Engineer = name, employeeID, email, github 
-//Intern = 
+//Intern = name, employeeID, email, school
 
 
 
-
-  inquirer
+function employeeData() {
+  return inquirer
   .prompt([
     {
+      type: 'list',
+      message: 'What type of employee?',
+      name: 'employeeType',
+      choices: ['manager', 'engineer', 'intern'],
+    },
+    {
       type: 'input',
-      message: 'What is the team memebers name?',
+      message: 'What is the employee name?',
       name: 'name',
     },
     {
@@ -39,28 +58,21 @@ const manager = require('./lib/manager');
       type: 'input',
       message: 'What is the email address?',
       name: 'email',
-    },
-    {
-      type: 'input',
-      message: 'What is your LinkedIn URL?',
-      name: 'linkedin',
-    },
-    {
-      type: 'input',
-      message: 'What is your GitHub URL?',
-      name: 'github',
-    },
-    {
-      type: 'input',
-      message: 'What is your Image URL?',
-      name: 'image',
-    },
+    }
+
   ])
-  .then((data) => {
-    console.log(data.name)
-    console.log(data.bio)
-    console.log(data.github)
+  
+}
 
-    writeToFile(userData(data));
-  })
+async function init () {
 
+let data = await employeeData()
+ console.log(data)
+
+ if(data.employeeType === "manager") {
+  console.log("this is a manager")
+ }
+
+}
+
+init()
