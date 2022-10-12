fcbb515520abb708d61246a858874f77dace04ea
tarekmn <nytarek@gmail.com>
2022-10-12 09:37:40 -0500

    test1 passed

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,29 +1,29 @@
 const Employee = require('../lib/employee');
 
-describe("Employee", ()=> {
-  describe("name"), () => {
-    it("should return the given name perimeter"), () => {
+describe("Employee", () => {
+  describe("name", () => {
+    it("should return the given name perimeter", () => {
       const template = new Employee("name", "employeeID", "email"); //Arrange
-      const result = template.getname() //Act getting the information
+      const result = template.name //Act getting the information
       expect(result).toEqual('name') //Assert 
-    }
-  }
+    })
+  })
+
+
+  describe("employeeID", () => {
+    it("should return the given ID perimeter", () => {
+      const template = new Employee("name", "employeeID", "email"); //Arrange
+      const result = template.employeeID //Act getting the information
+      expect(result).toEqual('employeeID') //Assert 
+    })
+  })
+
+  describe("email", () => {
+    it("should return the given email perimeter", () => {
+      const template = new Employee("name", "employeeID", "email"); //Arrange
+      const result = template.email //Act getting the information
+      expect(result).toEqual('email') //Assert 
+    })
+  })
+
 })
-
-describe("employeeID"), () => {
-  it("should return the given ID perimeter"), () => {
-    const template = new Employee("name", "employeeID", "email"); //Arrange
-    const result = template.getID() //Act getting the information
-    expect(result).toEqual('employeeID') //Assert 
-  }
-}
-
-
-
-
-// class Employee {        //***capital or not?? */
-//   constructor(name, employeeID, email) {
-//     this.name = name;
-//     this.employeeID = employeeID;
-//     this.email= email;
-//   }
