3e009d689de70aa9b56664c04a895f43544b439e
tarekmn <nytarek@gmail.com>
2022-10-12 17:17:24 -0500

    returning just one value but using loop

--- htmldata.js
+++ htmldata.js
@@ -6,7 +6,6 @@
 
 
 function htmlData(dataArray) {
-  
 
   let htmlTop = `<!DOCTYPE html>
   <html lang="en">
@@ -14,6 +13,7 @@ function htmlData(dataArray) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="stylesheet" href="./dist/practicestyle.css">
     <title>My Team</title>
@@ -27,12 +27,10 @@ function htmlData(dataArray) {
     
     </div>
   
-  
-  
     <div class="container-fluid">
       <div class="row cardRow"> `
 
-      let htmlBottom= `
+  let htmlBottom = `
       </div>
     </div>
   
@@ -42,14 +40,7 @@ function htmlData(dataArray) {
   </body>
   </html> `
 
-
-  return
-  htmlTop
-  
-      for  (let i = 0; i < dataArray.length; i++) {
-    $('.cardRow').append(
-          `
-      <div class="card">
+  function appender() {  for (let i = 0; i < dataArray.length; i++) { return `<div class="card">
         <img src="./Assets/img_avatar.png" alt="Avatar" style="width:100%">
         <div class="container">
           <h4><b>${dataArray[i].name}</b></h4>
@@ -61,12 +52,16 @@ function htmlData(dataArray) {
             </ul>
         </div>
       </div>`
-        ) 
-    } 
-htmlBottom
-
-        
     
+  }}
+
+
+  return htmlTop + appender() + htmlBottom;
+
+  
+
+
+
 }
 
 
